7c85f3bc08eeef64dac61d91ec4600303e49f615
WissemJderi <asqr43641@gmail.com>
2025-10-11 20:09:14 +0100

    Added a link to the focusnote demo video

--- components/main-content/data/myProjects.js
+++ components/main-content/data/myProjects.js
@@ -4,7 +4,7 @@ export const myProjects = [
     projectDescription: "A minimalist note-taking app for the terminal.",
     imageSrc: "focus_note.png",
     imageAlt: "Picture of the author",
-    liveDemo: "",
+    liveDemo: "https://youtu.be/8B_Ew92C02E",
     repo: "https://github.com/WissemJderi/Focus-Note.git",
   },
 ];
