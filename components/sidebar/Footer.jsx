587b21392454c05c6b588b60c53ce07e5ba1b889
WissemJderi <asqr43641@gmail.com>
2026-01-22 09:02:23 +0100

    Updated dep

--- components/sidebar/Footer.jsx
+++ components/sidebar/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
     },
   ];
   return (
-    <div className="text-center font-bbh text-white hidden sm:block px-4 sm:px-0">
+    <div className="text-center font-montserrat text-white hidden sm:block px-4 sm:px-0">
       <p className="text-2xl ">Get In Touch</p>
       <p className="text-sm text-gray-300 mt-1">
         Let's turn your vision into reality. Reach out and let's start building
